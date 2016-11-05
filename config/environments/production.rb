b34ed8ec254ba8b11b1aee8f4fd79a06fd12a5ce
louisscruz <louisstephancruz@me.com>
2016-11-04 22:21:47 -0700

    Attempt websocket production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'wss://knacklive.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'https://knacklive.herokuapp.com',
+    /http:\/\/yourappname.herokuapp.com.*/,
+    'http://www.knack.live',
+    /http:\/\/www.knack.live.*/
+  ]
 end
