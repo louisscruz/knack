099b467f85ab7a72e9f6b075fa4acd3a16a813c6
louisscruz <louisstephancruz@me.com>
2016-11-04 23:48:29 -0700

    Remove redis initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # config.action_cable.url = 'ws://www.knack.live/cable'
+  config.web_socket_server_url = 'wss://knacklive.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [
     'https://knacklive.herokuapp.com',
     /http:\/\/knacklive.herokuapp.com.*/,
